be4eaa81ed2412089e827c900a269b0521daae2b
Matthias Faust <syd711@gmail.com>
2026-07-10 13:55:02 +0200

    Initial commit

---
+++ a12-studio-ui/src/main/java/de/a12/studio/App.java
@@ -0,0 +1,8 @@
+package de.a12.studio;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("a12-studio-ui");
+    }
+}
---
+++ a12-studio-ui/src/test/java/de/a12/studio/AppTest.java
@@ -0,0 +1,13 @@
+package de.a12.studio;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class AppTest {
+
+    @Test
+    void appExists() {
+        assertNotNull(new App());
+    }
+}
