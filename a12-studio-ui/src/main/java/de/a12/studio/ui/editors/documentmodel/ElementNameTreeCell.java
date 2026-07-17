0fd920fbbe3922f31da7a804393fe352f1c6de58
Matthias Faust <syd711@gmail.com>
2026-07-17 22:02:02 +0200

    UI building

---
+++ a12-studio-ui/src/main/java/de/a12/studio/ui/editors/documentmodel/ElementNameTreeCell.java
@@ -0,0 +1,78 @@
+package de.a12.studio.ui.editors.documentmodel;
+
+import de.a12.studio.commons.util.WidgetFactory;
+import de.a12.studio.ui.util.Icons;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.control.TreeTableCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+class ElementNameTreeCell extends TreeTableCell<ElementViewModel, String> {
+
+  @Override
+  protected void updateItem(String name, boolean empty) {
+    super.updateItem(name, empty);
+    if (empty || name == null) {
+      setText(null);
+      setGraphic(null);
+      getStyleClass().remove("validation-error");
+      return;
+    }
+
+    ElementViewModel viewModel = getTableRow().getItem();
+    if (viewModel == null) {
+      setText(name);
+      setGraphic(null);
+      getStyleClass().remove("validation-error");
+    }
+    else {
+      Node icon = viewModel.isGroup()
+          ? WidgetFactory.createIcon(viewModel.getIcon())
+          : createElementIcon(viewModel);
+      icon.getStyleClass().add("tree-icon");
+
+      Label nameLabel = new Label(name);
+      nameLabel.getStyleClass().add("tree-cell-name-label");
+      HBox graphic = new HBox(4, icon, nameLabel);
+      graphic.setAlignment(Pos.CENTER_LEFT);
+      if (viewModel.hasAnnotations()) {
+        Node annotationIcon = WidgetFactory.createIcon(Icons.ELEMENT_ANNOTATION);
+        annotationIcon.getStyleClass().addAll("tree-icon", "tree-icon-badge");
+        Tooltip.install(annotationIcon, WidgetFactory.createTooltip("Element has annotations"));
+        graphic.getChildren().add(annotationIcon);
+      }
+      if (viewModel.isRequired()) {
+        Node requiredIcon = WidgetFactory.createIcon(Icons.ELEMENT_REQUIRED);
+        requiredIcon.getStyleClass().addAll("tree-icon", "tree-icon-badge");
+        Tooltip.install(requiredIcon, WidgetFactory.createTooltip("Required element"));
+        graphic.getChildren().add(requiredIcon);
+      }
+      setText(null);
+      setGraphic(graphic);
+
+      if (viewModel.hasError()) {
+        if (!getStyleClass().contains("validation-error")) {
+          getStyleClass().add("validation-error");
+        }
+      }
+      else {
+        getStyleClass().remove("validation-error");
+      }
+    }
+  }
+
+  private Node createElementIcon(ElementViewModel viewModel) {
+    String iconPath = viewModel.getIconPath();
+    return iconPath != null ? createPngIcon(iconPath) : WidgetFactory.createIcon(viewModel.getIcon());
+  }
+
+  private Node createPngIcon(String path) {
+    Image image = new Image(getClass().getResourceAsStream(path),
+        WidgetFactory.DEFAULT_ICON_SIZE, WidgetFactory.DEFAULT_ICON_SIZE, true, true);
+    return new ImageView(image);
+  }
+}
